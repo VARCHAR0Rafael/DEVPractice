7b0eb52b359adaabf2006a14313968462c09ae57
VARCHAR0Rafael <rtorresc@unah.hn>
2022-11-02 16:01:33 -0600

    added new power ups
    -add speedboost power up
    -add damageboost power up

---
+++ Hack n Slash/Hack n Slash Proyect/Assets/PowerUps/PowerUp_Speed.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp_Speed : Powerups
+{
+    [Header("Settings")]
+
+    public float amount = 6f;
+    public float duration = 5f;
+    public GameObject pickUpEffect;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            StartCoroutine(PickUp(collision));
+        }
+    }
+
+    IEnumerator PickUp(Collider2D player)
+    {
+
+        //Instantiate(pickUpEffect, transform.position, transform.rotation);
+        Player playerstats = player.GetComponent<Player>();
+        playerstats.speed += amount;
+
+        GetComponent<Collider2D>().enabled = false;
+        GetComponent<SpriteRenderer>().enabled = false;
+
+        yield return new WaitForSeconds(duration);
+
+        playerstats.speed -= amount;
+
+        Destroy(gameObject);
+    }
+}
